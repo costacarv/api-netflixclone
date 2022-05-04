b126146bdb8133eb6989c1e8373d23c795c6c291
Matheus Carvalho <matheucostamcc@hotmail.com>
2022-05-04 09:45:23 -0300

    api-netflix-final

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Header.css';
+
+export default function Header ({black}) {
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia" alt="netflix"></img>
+                </a>
+            </div>
+        <div className="header--user">
+            <a href="/">
+                <img src="https://pbs.twing.com/profile_images" alt="usuario"></img>
+            </a>
+        </div>
+        </header>
+    )
+}
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import './MovieRow.css'
+import React, { useState } from 'react';
+import './MovieRow.css';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
-export default ({title, items}) => {
+export default function MovieRow ({title, items}){
+    const [scrollX, setScrollx] = useState(0);
+
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth/2)
+        if(x>0) {
+            x=0
+        }
+        setScrollx(x)
+    }
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth/2);
+        let listW = items.results.length * 150;
+        if((window.innerWidth - listW) > x) {
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollx(x)
+    }
     return (
-        <div>
+        <div className='movieRow'>
             <h2>{title}</h2>
+            <div className='movieRow--left' onClick={handleLeftArrow}>
+                <NavigateBeforeIcon style={{fontSize: 50}}/>
+            </div>
+            <div className='movieRow--rigth' onClick={handleRightArrow}>
+            <NavigateNextIcon style={{fontSize: 50}}/>
+            </div>
             <div className='movieRow--listarea'>
-                {items.results.length > 0 && items.results.map((item, key) => (
-                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
+            <div className='movieRow--list' style={{marginLeft: scrollX, width: items.results.length * 150}}>
+                {items.results.length > 0 && items.results.map((movie, key) => (
+                    <div key={key} className='movieRow--item'>
+                    <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.original_title}/>
+                    </div>
                 ))}
             </div>
+        </div>
         </div>
     )
 }
